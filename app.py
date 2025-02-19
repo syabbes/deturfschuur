a0a3ea26cb999ef92ae9e9893c05cb4bf25da926
syabbes <simonabbes@gmail.com>
2025-02-19 23:38:22 +0100

    Add app.py and helpers.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from datetime import timedelta
+from helpers import login_required
+
+# Configure application
+app = Flask(__name__)
+
+#secret key
+app.secret_key = b'\x07)\x11\x08\xc8Z|u`*\xefG#\x88>J\xb7\x12\xbe\x99[\xf7\x88+'
+
+#configure session so users stay logged in for the duration of the browser
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+@login_required
+def index():
+    # TODO
+    return render_template("index.html")
