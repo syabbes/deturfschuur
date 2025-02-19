d1ef3919916ace7d0a1d8d81604071d8209d2ad7
syabbes <simonabbes@gmail.com>
2025-02-19 23:45:16 +0100

    changing things in app and helpers

---
+++ helpers.py
@@ -0,0 +1,19 @@
+from flask import redirect, render_template, session
+from functools import wraps
+
+print("helpers.py is being imported")
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/latest/patterns/viewdecorators/
+    """
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+
+    return decorated_function
